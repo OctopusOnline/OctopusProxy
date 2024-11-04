00a226f74d2a23bae345793ad272e0ac8085bfa0
Clemens Banowski <officialeta09@gmail.com>
2024-11-04 21:27:06 +0100

    removed useless super constructor

--- packages/server/dist/service/prisma.service.js
+++ packages/server/dist/service/prisma.service.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -22,9 +19,6 @@ exports.PrismaService = void 0;
 const common_1 = require("@nestjs/common");
 const client_1 = require("@prisma/client");
 let PrismaService = class PrismaService extends client_1.PrismaClient {
-    constructor() {
-        super();
-    }
     onModuleInit() {
         return __awaiter(this, void 0, void 0, function* () {
             yield this.$connect();
@@ -38,7 +32,6 @@ let PrismaService = class PrismaService extends client_1.PrismaClient {
 };
 exports.PrismaService = PrismaService;
 exports.PrismaService = PrismaService = __decorate([
-    (0, common_1.Injectable)(),
-    __metadata("design:paramtypes", [])
+    (0, common_1.Injectable)()
 ], PrismaService);
 //# sourceMappingURL=prisma.service.js.map
