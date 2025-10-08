21f322fd549516e3866969623af083454220b786
Clemens Banowski <officialeta09@gmail.com>
2025-10-08 02:05:24 +0200

    added prisma generate to entrypoint.sh

--- images/server/src/entrypoint.sh
+++ images/server/src/entrypoint.sh
@@ -4,11 +4,14 @@ if [ -n "$DATABASE_URL" ]; then
   export PRISMA_DATABASE_URL=${DATABASE_URL/mariadb:/mysql:}
 fi
 
+echo "Generating prisma client..."
+npm run prisma:generate --prefix ./node_modules/@octopusproxy/server
+
 echo "Running database migrations..."
 npm run prisma:migrate --prefix ./node_modules/@octopusproxy/server
 
 if [ "$DEBUG" == "true" ]; then
-  exec node --inspect=0.0.0.0:${DEBUG_PORT:-9229} index.js
+  exec node --inspect="0.0.0.0:${DEBUG_PORT:-9229}" index.js
 else
   exec node index.js
 fi
